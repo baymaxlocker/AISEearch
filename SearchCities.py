94d5abb7c0c2aa4e36566b568a612c93ab0fe3b0
baymaxlocker <45436406+baymaxlocker@users.noreply.github.com>
2021-07-27 14:43:17 -0400

    Update SearchCities.py

--- SearchCities.py
+++ SearchCities.py
@@ -6,15 +6,15 @@ import pandas as pd
 import re
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 
 
 def open_file(in_file,searchType):
 	roadsList = []
+	lat_lon_list = []
 
 	with open(in_file, 'r') as words:
 
-		lines = words
-		
 		if searchType == "bfs" or searchType == "dfs":
  #      for number, line in enumerate(lines, start = 6):
 			for line, words in enumerate(words):
@@ -24,22 +24,83 @@ def open_file(in_file,searchType):
 					myString = re.sub(r"[\n\t\s]*", "", mystring)
 					roadsList.append(myString.strip().split(','))
 
-		if searchType == "A*":        
+		if searchType == "A*":
+
 			for line, words in enumerate(words):
-			
-				if line > 5 and line <71:
+				if line > 5 and line < 71:
+					mystring = words
+					myString = re.sub(r"[\n\t\s]*", "", mystring)
+					lat_lon_list.append(myString.strip().split(','))
+
+				if line > 74:
 					mystring = words
 					myString = re.sub(r"[\n\t\s]*", "", mystring)
 					roadsList.append(myString.strip().split(','))
+
+			roads_allinfo = combine(roadsList,lat_lon_list)
+#		print("Here's the road list: "+str(roadsList))
 		
-		print("Here's the road list: "+str(roadsList))
+		print(" Full list: "+str(roads_allinfo))
 		
-	return roadsList
+	return roads_allinfo
+	
+	# Returns a master list with all the info in it
+def combine(list1,list2):
+	
+		# Add lat lon to first city
+		for i in list1:
+			for j in list2:
+				if i[0] == j[0]:
+					i.append(j[1])
+					i.append(j[2])
+
+		# Add lat lon to second city		
+		for k in list1:
+			for l in list2:
+				if k[1] == l[0]:
+					k.append(l[1])
+					k.append(l[2])
+
+		# Add heuristic value between cities to list
+		for comb in list1:
+			comb.append(heuristic(float(comb[3]),float(comb[4]),float(comb[5]),float(comb[6])))
+			#comb.append(heuristic(comb[3],comb[4],comb[5],comb[6]))
+
+		# Add total cost H(n) + G(n) to list
+		for tot in list1:
+			hTotal = float(tot[2]) + float(tot[7])
+			tot.append(hTotal)
+			#comb.append(heuristic(comb[3],comb[4],comb[5],comb[6]))
+
+		combined = list1
+		
+		return combined
+
+# Returns heuristic value between source and a target cities given their geolocation
+def heuristic(lat1,lon1,lat2,lon2):
+
+#--
+# Heuristic formula
+# sqrt((69.5 * (Lat1 - Lat2)) ^ 2 + (69.5 * cos((Lat1 + Lat2)/360 * pi) * (Long1 - Long2)) ^ 2)
+#--
+
+	hval = math.sqrt(pow((69.5 * (lat1 - lat2)), 2) + pow(69.5 * math.cos((lat1 + lat2)/360 * math.pi) * (lon1 - lon2),2))
+
+	return hval
 
 
 def create_tree(node_list):
 	
 	df = pd.DataFrame(node_list, columns=['City1', 'City2', 'KM'])
+	#call heuristic function here
+	G = nx.from_pandas_edgelist(df, 'City1', 'City2')
+	G = nx.from_pandas_edgelist(df, 'City1', 'City2', edge_attr='KM')
+	
+	return G
+
+def create_tree_heuristics(node_list):
+	
+	df = pd.DataFrame(node_list, columns=['City1', 'City2', 'KM','Lat1','Lon1','Lat2','Lon2','H(n)','H(n)+G(n)'])
 	#call heuristic function here
 	G = nx.from_pandas_edgelist(df, 'City1', 'City2')
 	G = nx.from_pandas_edgelist(df, 'City1', 'City2', edge_attr='KM')
@@ -113,7 +174,7 @@ class Search(Node):
 							print("The stack is "+str(self.stack))
 							self.iteration+=1
 
-	def astar(self,f):
+	def astar(self,startcity, goalcity):
 		print("Group 7 is awesome!")
 
 # procedure DFS_iterative(G, v) is
@@ -183,14 +244,19 @@ if __name__ == "__main__":
 	StartCity = "albanyNY"
 	GoalCity = "omaha"
 	#type_of_search = "bfs"
-	type_of_search = "dfs"
-	#type_of_search = "A*"
+	#type_of_search = "dfs"
+	type_of_search = "A*"
 
 	road_list = open_file(in_file,type_of_search)
 
 	print("Starting Node: " + StartCity)
 	
-	G = create_tree(road_list)
+	if type_of_search == "bfs" or type_of_search=="dfs":
+		G = create_tree(road_list)
+
+	elif type_of_search == "A*":
+		G = create_tree_heuristics(road_list)
+	
 	callingSearch(StartCity, GoalCity, type_of_search)
 
 	
