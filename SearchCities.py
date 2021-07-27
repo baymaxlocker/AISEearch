0def29271d7621401f559fcb28c1f67ea92e3dca
baymaxlocker <45436406+baymaxlocker@users.noreply.github.com>
2021-07-27 11:18:23 -0400

    Update SearchCities.py

--- SearchCities.py
+++ SearchCities.py
@@ -8,104 +8,113 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def open_file(in_file):
-    roadsList = []
+def open_file(in_file,searchType):
+	roadsList = []
 
-    with open(in_file, 'r') as words:
-#        for line in words:
-        lines = words
-        
+	with open(in_file, 'r') as words:
+
+		lines = words
+		
+		if searchType == "bfs" or searchType == "dfs":
  #      for number, line in enumerate(lines, start = 6):
-        for line, words in enumerate(words):
-            
-            if line > 5 and line <71:
-                mystring = words
-                myString = re.sub(r"[\n\t\s]*", "", mystring)
-                roadsList.append(myString.strip().split(','))
-        
-        print("Here's the road list: "+str(roadsList))
-        
-    return roadsList
+			for line, words in enumerate(words):
+			
+				if line > 74:
+					mystring = words
+					myString = re.sub(r"[\n\t\s]*", "", mystring)
+					roadsList.append(myString.strip().split(','))
+
+		if searchType == "A*":        
+			for line, words in enumerate(words):
+			
+				if line > 5 and line <71:
+					mystring = words
+					myString = re.sub(r"[\n\t\s]*", "", mystring)
+					roadsList.append(myString.strip().split(','))
+		
+		print("Here's the road list: "+str(roadsList))
+		
+	return roadsList
 
 
 def create_tree(node_list):
-    
-    df = pd.DataFrame(node_list, columns=['City1', 'City2', 'KM'])
-    #call heuristic function here
-    G = nx.from_pandas_edgelist(df, 'City1', 'City2')
-    G = nx.from_pandas_edgelist(df, 'City1', 'City2', edge_attr='KM')
-    
-    return G
+	
+	df = pd.DataFrame(node_list, columns=['City1', 'City2', 'KM'])
+	#call heuristic function here
+	G = nx.from_pandas_edgelist(df, 'City1', 'City2')
+	G = nx.from_pandas_edgelist(df, 'City1', 'City2', edge_attr='KM')
+	
+	return G
 
 
 class Node(object):
-    def __init__(self, parent, name):
-        #self.parent = parent
-        self.name = name
+	def __init__(self, parent, name):
+		#self.parent = parent
+		self.name = name
 
 
 class Search(Node):
-    def __init__(self):
-        self.visited = []
-        self.queue = []
-        self.weightedQueue = {}
-        self.newNodes = []
-        self.neighborL = []
-        self.return_path = []
-        self.stack=[]
+	def __init__(self):
+		self.visited = []
+		self.queue = []
+		self.weightedQueue = {}
+		self.newNodes = []
+		self.neighborL = []
+		self.return_path = []
+		self.stack=[]
 
-    def bfs(self, node, goalCity):
-        self.visited.append(node)
-        self.queue.append(node)
-        self.node = str(node)
-        self.neighborList = (G.adj[node])
-        while self.queue:
-            s = self.queue.pop(0)
-            print("City to expand is: "+str(s))
-            nList = self.getNeighbor(s)
-            if (s == goalCity):
-                        #print("These are visited nodes\n >>>", self.visited, "\n")
-                        
-                        print("Found:", goalCity,"")
-                        self.return_path.append(goalCity)
-                        self.find_path(goalCity)
-                        print("Return path is "+str(self.return_path))
-                        break
-            else:
-                self.visited.append(s)  
-                for neighbor in nList:
-                    if neighbor not in self.visited and neighbor not in self.queue:
-                        self.queue.append(neighbor)
-                        print("The stack is "+str(self.queue))
-     
-    def dfs(self, node, goalCity):
-        self.visited.append(node)
-        self.stack.append(node)
-        self.node = str(node)
-        self.neighborList = (G.adj[node])
-        self.iteration = 0
-        while self.stack:
-            s = self.stack.pop()
-            nList = self.getNeighbor(s)
-            if (s == goalCity):
-             #print("These are visited nodes\n >>>", self.visited, "\n")
-                   
-                print("Found:", goalCity,"")
-                self.return_path.append(goalCity)
-                self.find_path(goalCity)
-                print("Return path is "+str(self.return_path))
-                break
-                
-            else:
-                self.visited.append(s)  
-                for neighbor in nList:
-                        if neighbor not in self.visited and neighbor not in self.stack:
-                            self.stack.append(neighbor)
-                            print("The stack is "+str(self.stack))
-                            self.iteration+=1
+	def bfs(self, node, goalCity):
+		self.visited.append(node)
+		self.queue.append(node)
+		self.node = str(node)
+		self.neighborList = (G.adj[node])
+		while self.queue:
+			s = self.queue.pop(0)
+			print("City to expand is: "+str(s))
+			nList = self.getNeighbor(s)
+			if (s == goalCity):
+						#print("These are visited nodes\n >>>", self.visited, "\n")
+						
+						print("Found:", goalCity,"")
+						self.return_path.append(goalCity)
+						self.find_path(goalCity)
+						print("Return path is "+str(self.return_path))
+						break
+			else:
+				self.visited.append(s)  
+				for neighbor in nList:
+					if neighbor not in self.visited and neighbor not in self.queue:
+						self.queue.append(neighbor)
+						print("The stack is "+str(self.queue))
+	 
+	def dfs(self, node, goalCity):
+		self.visited.append(node)
+		self.stack.append(node)
+		self.node = str(node)
+		self.neighborList = (G.adj[node])
+		self.iteration = 0
+		while self.stack:
+			s = self.stack.pop()
+			nList = self.getNeighbor(s)
+			if (s == goalCity):
+			 #print("These are visited nodes\n >>>", self.visited, "\n")
+				   
+				print("Found:", goalCity,"")
+				self.return_path.append(goalCity)
+				self.find_path(goalCity)
+				
+				break
+				
+			else:
+				self.visited.append(s)  
+				for neighbor in nList:
+						if neighbor not in self.visited and neighbor not in self.stack:
+							self.stack.append(neighbor)
+							print("The stack is "+str(self.stack))
+							self.iteration+=1
 
-    def astar(self,f):
-        print("Group 7 is awesome!")
+	def astar(self,f):
+		print("Group 7 is awesome!")
 
 # procedure DFS_iterative(G, v) is
 #     let S be a stack
@@ -121,64 +130,73 @@ class Search(Node):
 
 
 
-    def getNeighbor(self, CurrNode):
-        new_nodes = []
-        inpt = str(CurrNode)
-        neighbor = list(G.adj[inpt])
-        for n in neighbor:
-            if n not in self.queue:
-                if n not in self.visited:
-                    new_nodes.append(n)
-                    
-        newNeighbor = new_nodes
-        self.neighborL.append(new_nodes)
-        return newNeighbor    
+	def getNeighbor(self, CurrNode):
+		new_nodes = []
+		inpt = str(CurrNode)
+		neighbor = list(G.adj[inpt])
+		for n in neighbor:
+			if n not in self.queue:
+				if n not in self.visited:
+					new_nodes.append(n)
+					
+		newNeighbor = new_nodes
+		self.neighborL.append(new_nodes)
+		return newNeighbor    
 
 
-    def find_path(self, goalCity):
-        currNode = goalCity
-        neighbor = list(G.adj[currNode])
-        
-        for n in neighbor:
-            if n == self.visited[0]:
-                self.return_path.insert(0, n)
-                print("Here is the path to the goal")
-                print(self.return_path)
-                break
-         
-            if n in self.visited and n not in self.return_path:
-                self.return_path.insert(0, n)
-                self.find_path(n)
-                if self.return_path[0] == self.visited[0]:
-                    break
-            
+	def find_path(self, goalCity):
+		currNode = goalCity
+		neighbor = list(G.adj[currNode])
+		
+		for n in neighbor:
+			if n == self.visited[0]:
+				self.return_path.insert(0, n)
+				print("Here is the path to the goal: ")
+				print(self.return_path)
+				break
+		 
+			if n in self.visited and n not in self.return_path:
+				self.return_path.insert(0, n)
+				self.find_path(n)
+				if self.return_path[0] == self.visited[0]:
+					break
+			
 
 
 
 def callingSearch(startCity, goalCity, typeOfSearch):
-    g = Search()
-    if typeOfSearch == "bfs":
-        g.bfs(startCity, goalCity)   
-    
-    elif typeOfSearch == "dfs":
-        g.dfs(startCity, goalCity) 
+	g = Search()
+	if typeOfSearch == "bfs":
+		g.bfs(startCity, goalCity)   
+	
+	elif typeOfSearch == "dfs":
+		g.dfs(startCity, goalCity) 
+
+	elif typeOfSearch == "A*":
+		g.astar(startCity, goalCity) 
 
 
 if __name__ == "__main__":
-    
-    in_file = "Cities.txt"
+	
+	in_file = "Cities.txt"
 
-    StartCity = "albanyNY"
-    GoalCity = "omaha"
-    type_of_search = "bfs"
-    #type_of_search = "dfs"
+	StartCity = "albanyNY"
+	GoalCity = "omaha"
+	#type_of_search = "bfs"
+	type_of_search = "dfs"
+	#type_of_search = "A*"
 
-    road_list = open_file(in_file)
-    G = create_tree(road_list)
-    print("Data frame is :"+str(G))
-    nx.draw(G,
-              with_labels=True,
-              arrows=True)
-    plt.show()
-    print("Starting Node: " + StartCity)
-    callingSearch(StartCity, GoalCity, type_of_search)
+	road_list = open_file(in_file,type_of_search)
+
+	print("Starting Node: " + StartCity)
+	
+	G = create_tree(road_list)
+	callingSearch(StartCity, GoalCity, type_of_search)
+
+	
+	#print("Data frame is :"+str(G))
+	
+	nx.draw(G,
+			  with_labels=True,
+			  arrows=True)
+	plt.show()
